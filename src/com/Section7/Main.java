e249ac9fb2d5e80a7f0bd6c64d18ebdb0a95d209
Bobby Smalls <bsmalls05@hotmail.com>
2019-04-05 14:54:00 -0400

    Change the boolean to false from true

--- src/com/Section7/Main.java
+++ src/com/Section7/Main.java
@@ -30,7 +30,7 @@ public class Main {
 		System.out.println("Initial health is " + player.getHealth());
 		System.out.println("================================================");
 
-		Printer printer = new Printer(50, true);
+		Printer printer = new Printer(50, false);
 		System.out.println("initial page count = " + printer.getPagesPrinted());
 		int pagesPrinted = printer.printPages(4);
 		System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
