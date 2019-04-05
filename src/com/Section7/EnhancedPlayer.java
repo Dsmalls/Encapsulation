b5b5cd5ee61542b5fd81821cfd97861da3c41098
Bobby Smalls <bsmalls05@hotmail.com>
2019-04-05 14:48:36 -0400

    Initial commit

---
+++ src/com/Section7/EnhancedPlayer.java
@@ -0,0 +1,33 @@
+package com.Section7;
+
+public class EnhancedPlayer {
+
+
+
+    private String name;
+    private int hitPoints =  100;
+    private String weapon;
+
+    // here I am securing the name, hitPoints and weapon will be initialized when the class is created
+    public EnhancedPlayer(String name, int health, String weapon) {
+        this.name = name;
+
+        if (health > 0 && health <= 100){
+            this.hitPoints = health;
+        }
+
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        this.hitPoints = this.hitPoints - damage;
+        if (this.hitPoints <= 0) {
+            System.out.println("Player knocked out");
+            // Reducing the number of lives remaining for the player
+        }
+    }
+
+    public int getHealth() {
+        return hitPoints;
+    }
+}
---
+++ src/com/Section7/Main.java
@@ -0,0 +1,42 @@
+package com.Section7;
+
+public class Main {
+	// Encapsulation in Java is a mechanism of wrapping the data (variables) and code
+	// acting on the data (methods) together as a single unit.
+	// In encapsulation, the variables of a class will be hidden from other classes, and can be accessed
+	// only through the methods of their current class. Therefore, it is also known as data hiding.
+
+	// this is an example of improper encapsulation
+	// I can change the fields where it will override the commands from the player class
+    public static void main(String[] args) {
+//	Player player = new Player();
+//	player.name = "Keith";
+//	player.health = 25;
+//	player.weapon = "Sword";
+//
+//	int damage = 10;
+//	player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 12;
+//		player.loseHealth(damage);
+//		System.out.println("Remaining health = " + player.healthRemaining());
+//
+//		damage = 15;
+//		player.loseHealth(damage);
+//		System.out.println("Remaining health = " + player.healthRemaining());
+
+		EnhancedPlayer player = new EnhancedPlayer("Dez", 50, "Sword");
+		System.out.println("Initial health is " + player.getHealth());
+		System.out.println("================================================");
+
+		Printer printer = new Printer(50, true);
+		System.out.println("initial page count = " + printer.getPagesPrinted());
+		int pagesPrinted = printer.printPages(4);
+		System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+		pagesPrinted = printer.printPages(5);
+		System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+
+
+    }
+}
